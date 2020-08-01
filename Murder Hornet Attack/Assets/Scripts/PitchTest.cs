dfe93145dc732d40197047e6b21eae3067af275b
winchell459 <johncmorris@gmail.com>
2020-08-01 10:56:45 -0700

    pitchTest

---
+++ Murder Hornet Attack/Assets/Scripts/PitchTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitchTest : MonoBehaviour
+{
+    private Rigidbody2D Player;
+    public float smoothing = 1;
+    public float MaxSpeed, MinSpeed;
+    public float MaxPitch, MinPitch;
+    public AudioSource AS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AS = GetComponent<AudioSource>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (!Player) Player = FindObjectOfType<HornetController>().GetComponent<Rigidbody2D>();
+        else
+        {
+            Debug.Log(Player.velocity.magnitude);
+            float speed = Player.velocity.magnitude;
+            float percent = (speed - MinSpeed) / (MaxSpeed - MinSpeed);
+            float pitch = percent * (MaxPitch - MinPitch) + MinPitch;
+            pitch = pitch < MinPitch ? MinPitch : pitch;
+            AS.pitch = pitch;
+        }
+    }
+}
