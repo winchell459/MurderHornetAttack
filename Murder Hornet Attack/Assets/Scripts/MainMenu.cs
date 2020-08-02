59ebd56492a3cf48bf43357018974dad18d74a86
john morris <johncmorris@gmail.com>
2020-08-02 16:00:59 -0700

    PitchTest build

--- Murder Hornet Attack/Assets/Scripts/MainMenu.cs
+++ Murder Hornet Attack/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public Text TouchToPlayText;
+    public AudioSource ThemeSource;
     private float TouchToPlayDelay = 2;
     private float touchToPlayDelayStart;
     // Start is called before the first frame update
@@ -19,8 +20,18 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (TouchToPlayDelay + touchToPlayDelayStart < Time.fixedTime) TouchToPlayText.gameObject.SetActive(true);
+        if (TouchToPlayDelay + touchToPlayDelayStart < Time.fixedTime)
+        {
+            TouchToPlayText.gameObject.SetActive(true);
+            ThemeSource.Play();
+            touchToPlayDelayStart = float.PositiveInfinity;
+        }
 
-        if (Input.GetMouseButtonDown(0)) SceneManager.LoadScene(1);
+        if (Input.GetMouseButtonDown(0)) SceneManager.LoadScene(2);
     }
+
+    //IEnumerator startTheme()
+    //{
+
+    //}
 }
