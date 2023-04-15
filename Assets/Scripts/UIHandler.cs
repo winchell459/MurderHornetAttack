2eb5775b1b58174bc15d9a3eaf58c44b313fa342
winchell459 <johncmorris@gmail.com>
2023-04-14 22:05:23 -0700

    hide menu button added

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject[] menuPanels;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayMenu(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleMenu()
+    {
+        if (menuPanels.Length > 0) DisplayMenu(!menuPanels[0].activeSelf);
+    }
+    private void DisplayMenu(bool on)
+    {
+        foreach (GameObject menuPanel in menuPanels) menuPanel.SetActive(on);
+    }
+}
